ab492d73fdc708e2406468d9feab05949457546c
J-MU <alsdnrdl001@gmail.com>
2022-08-29 00:23:52 +0900

    회원가입 validation처리중 test를 위한 commit

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -22,17 +22,15 @@ module.exports = {
     SIGNIN_EMAIL_ERROR_TYPE : { "isSuccess": false, "code": 2010, "message":"이메일을 형식을 정확하게 입력해주세요." },
     SIGNIN_PASSWORD_EMPTY : { "isSuccess": false, "code": 2011, "message": "비밀번호를 입력 해주세요." },
 
-    USER_USERID_EMPTY : { "isSuccess": false, "code": 2012, "message": "userId를 입력해주세요." },
-    USER_NAME_EMPTY : { "isSuccess": false, "code": 2012, "message": "userName을 입력해주세요." },
+    USER_USERID_EMPTY : { "isSuccess": false, "code": 2001, "message": "userId를 입력해주세요." },
+    USER_NAME_EMPTY : { "isSuccess": false, "code": 2002, "message": "userName을 입력해주세요." },
     USER_USERID_NOT_EXIST : { "isSuccess": false, "code": 2013, "message": "해당 회원이 존재하지 않습니다." },
 
-    USER_PROFILEIMG_EMPTY : { "isSuccess": false, "code": 2014, "message": "프로필 이미지를 입력해주세요." },
-    USER_USEREMAIL_NOT_EXIST : { "isSuccess": false, "code": 2015, "message": "해당 이메일을 가진 회원이 존재하지 않습니다." },
+    USER_PROFILEIMG_EMPTY : { "isSuccess": false, "code": 2003, "message": "프로필 이미지를 입력해주세요." },
     USER_ID_NOT_MATCH : { "isSuccess": false, "code": 2016, "message": "유저 아이디 값을 확인해주세요" },
-    USER_NICKNAME_EMPTY : { "isSuccess": false, "code": 2017, "message": "변경할 닉네임 값을 입력해주세요" },
 
-    USER_STATUS_EMPTY : { "isSuccess": false, "code": 2018, "message": "회원 상태값을 입력해주세요" },
 
+    USER_USERID_INVALID_VALUE:{"isSuccess": false, "code": 2010, "message": "userId값이 정상적인 값이 아닙니다."},
     //2050 : mission request error
     MISSION_GROUPID_EMPTY : { "isSuccess": false, "code": 2050, "message": "groupId를 입력해주세요." },
 
