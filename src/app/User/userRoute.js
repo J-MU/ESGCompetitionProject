43975d7242dd150cb585d43ec52e0a73336aed06
민욱 <alsdnrdl001@gmail.com>
2022-09-01 20:13:12 +0900

    friend domain 설정중

--- src/app/User/userRoute.js
+++ src/app/User/userRoute.js
@@ -59,13 +59,13 @@ module.exports = function(app){
     // 1. 유저 생성 (회원가입) API
     app.post('/app/users', user.postUsers);
 
-    // 2. 유저 조회 API (=유저 프로필 정보)
+    // 2. 유저 조회 API
     app.get('/app/users',user.getUsers); 
 
-    // 3. 특정 유저 조회 API
+    // 3. 특정 유저 조회 API (=유저 프로필 정보)
     app.get('/app/users/:userId', user.getUserById);
 
-
+    
     // TODO: After 로그인 인증 방법 (JWT)
     // 로그인 하기 API (JWT 생성)
     app.post('/app/login', user.login);
